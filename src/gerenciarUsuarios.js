90e9747a32a5f7f5a97ddb1ee692729efdd7cd81
Mafra <maframano@hotmail.com>
2025-09-09 22:56:38 -0300

    mudei a lista de nome de usuarios para tornarem um objeto

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,14 +1,53 @@
-const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+/*const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+
+//function retornarUsuarios() {
+//   return usuarios;
+//}
+
+//push é uma função que adiciona um novo item na lista usuarios
+//function adicionaNovoUsuario(nome){
+   // usuarios.push(nome);
+//}
+
+//module.exports = {
+  //  retornarUsuarios, adicionaNovoUsuario
+//}*/
+
+const usuarios = [
+    {nome : 'Matheus',
+     email :'matheus@hotmail.com'  
+    }, 
+    
+    {nome :'Marcos', 
+      email :'marcos@hotmail.com'   
+    },
+
+    {nome :'Lucas',
+      email :'lucas@hotmail.com'   
+    }, 
+    
+    {nome :'João',
+     email :'joao@hotmail.com' 
+    }
+];
 
 function retornarUsuarios() {
    return usuarios;
 }
 
 //push é uma função que adiciona um novo item na lista usuarios
-function adicionaNovoUsuario(nome){
-    usuarios.push(nome);
+function adicionaNovoUsuario(usuario){
+    usuarios.push(usuario);
 }
 
 module.exports = {
     retornarUsuarios, adicionaNovoUsuario
 }
+/*
+  não precisou declarar na função pois você tá 
+  indicando que essa função precisa de receber um dado, 
+  então ele meio que cria automaticamente nesse caso
+  você só cria a caixa (const) quando está dentro de um escopo, 
+  se for nos parâmetros, não é necessário pois ele cria 
+  automaticamente durante a execução
+*/ 
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,14 +4,30 @@ const {expect } = require('chai');
 describe('Testar as funções de Gestão de usuarios', function (){
     it('Validar que posso adicionar um novo nome de usuario na lista', function(){
         //1. adicionar um novo nome na lista de usuarios
-            adicionaNovoUsuario('Maria');
+            /*adicionaNovoUsuario('Maria');*/
+            adicionaNovoUsuario({
+                nome:'Maria',
+                email:'maria@gospel.com'
+
+    });
+
         //2. Retornar a lista de usuarios na caixa lista de usuarios
             const listaDeUsuarios = retornarUsuarios();
         //3. Comparar se o novo nome está no final da lista de usuarios
-            expect(listaDeUsuarios.at(-1)).to.equal('Maria');
-
+            //expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+            expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
+            expect(listaDeUsuarios.at(-1).email).to.equal('maria@gospel.com');
+           //ou
+            expect(listaDeUsuarios.at(-1)).to.eql({
+                nome: 'Maria',
+                email:'maria@gospel.com'
+            });
     });
 
 });
 
 //definir o passo a passo de tudo que preciso fazer
+
+/*to.equal compara com 1 propriedade
+to.eql compara o objeto todo
+*/
